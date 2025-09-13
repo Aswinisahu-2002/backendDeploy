f5002b1a27441865085bdf9ffc801e00394d43dc
Aswinisahu-2002 <aswinisahu904@gmail.com>
2025-09-13 16:13:09 +0530

    test case

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get("/demo",(req,res)=>{
     res.status(200).json({message:"App is running now"})
 })
 
+app.get("test",(req,res)=>{
+    res.status(200).json({message:"Test sussess"})
+})
+
 app.listen(process.env.PORT || 5050,()=>{
     console.log("SERVER IS RUNNING")
 })
