25210288b2b8845e26bcfdaeba185bcc8b2b5722
Aswinisahu-2002 <aswinisahu904@gmail.com>
2025-09-17 20:03:22 +0530

    databse

--- models/user_models.js
+++ models/user_models.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const user_schema = require("../schemas/user_shemas")
 
-const USER = mongoose.model("/user",user_schema)
+const USER = mongoose.model("user",user_schema)
 
 module.exports = USER
 
