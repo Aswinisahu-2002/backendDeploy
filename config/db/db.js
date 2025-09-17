e1aad6245bbbb3673f46e30bbc49b1b197d96eb6
Aswinisahu-2002 <aswinisahu904@gmail.com>
2025-09-17 20:25:57 +0530

    n

--- config/db/db.js
+++ config/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const connectDb = async (params) => {
+const connectDb = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URL)
         console.log("Database connected")
